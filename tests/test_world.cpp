41d029ab432a3a951d6f1023bf1df73322fa7acf
Kleascm <kleascm@gmail.com>
2025-03-19 00:37:36 +1000

    add dynamic and static bodies to test for PhysicsWorld update

--- tests/test_world.cpp
+++ tests/test_world.cpp
@@ -8,3 +8,14 @@
 void testWorldUpdate() {
     PhysicsWorld world;
     world.fixedDeltaTime = 1.0f; // For easy math (1 second)
+    RigidBody body1;
+    body1.SetMass(1.0f); // Dynamic
+    body1.position = Vector3(0, 10, 0);
+
+    RigidBody body2;
+    body2.SetMass(2.0f); // Dynamic
+    body2.position = Vector3(0, 20, 0);
+
+    RigidBody body3;
+    body3.SetMass(0.0f); // Static
+    body3.position = Vector3(0, 100, 0);
